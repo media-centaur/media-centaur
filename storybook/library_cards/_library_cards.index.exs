bb95900fb1d8196606cfdded4fa12a12fd310056
Shawn <shawn@mccool.email>
2026-05-02 19:55:46 +0200

    feat(storybook): add library_cards/toolbar story

--- storybook/library_cards/_library_cards.index.exs
+++ storybook/library_cards/_library_cards.index.exs
@@ -5,4 +5,5 @@ defmodule MediaCentarrWeb.Storybook.LibraryCards do
   def folder_icon, do: {:fa, "table-cells", :light, "psb:mr-1"}
 
   def entry("poster_card"), do: [icon: {:fa, "image", :thin}, name: "Poster card"]
+  def entry("toolbar"), do: [icon: {:fa, "sliders", :thin}, name: "Toolbar"]
 end
