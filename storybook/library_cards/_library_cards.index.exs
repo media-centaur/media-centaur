6df3ee9a866cb324209e2e0a256fc20144f6cefa
Shawn <shawn@mccool.email>
2026-05-02 19:43:14 +0200

    feat(storybook): add library_cards/poster_card story

---
+++ storybook/library_cards/_library_cards.index.exs
@@ -0,0 +1,8 @@
+defmodule MediaCentarrWeb.Storybook.LibraryCards do
+  use PhoenixStorybook.Index
+
+  def folder_open?, do: true
+  def folder_icon, do: {:fa, "table-cells", :light, "psb:mr-1"}
+
+  def entry("poster_card"), do: [icon: {:fa, "image", :thin}, name: "Poster card"]
+end
