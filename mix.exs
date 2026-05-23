8fb9b6d21a90394f9d78f7d99ea1da10897fb74e
Shawn McCool <shawn@mccool.email>
2026-05-23 17:49:01 +0200

    chore: bump version to 0.72.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule MediaCentaur.MixProject do
   def project do
     [
       app: :media_centaur,
-      version: "0.72.2",
+      version: "0.72.3",
       elixir: "~> 1.15",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
