b68508c4bc20e0a8779b1078fad3e22fcdb43cff
Shawn McCool <shawn@mccool.email>
2026-09-22 21:26:51 +0200

    chore: bump version to 1.37.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule MediaCentaur.MixProject do
   def project do
     [
       app: :media_centaur,
-      version: "1.36.0",
+      version: "1.37.0",
       elixir: "~> 1.20",
       elixirc_paths: elixirc_paths(Mix.env()),
       # `:permanent` makes the VM halt when the OTP application stops. Outside
